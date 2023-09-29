1bf3d83adc64a9a8a7ef7ae6260cd5c2f4bea719
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-29 17:13:14 +0100

    aprendendo sobre projecto

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def helloworld(request):
+    return HttpResponse('Hello World!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name' : name})
