ea5a19edc0a7bf0a8a79b0f3a34a7df09ecab444
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-02 19:58:58 +0100

    messages alert

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 def taskList(request):
     tasks = Task.objects.all
@@ -42,6 +43,9 @@ def editTask(request, id):
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
+
+    messages.info(request, 'Tarefa deletada com sucesso')
+
     return redirect('/')
 
 def helloworld(request):
