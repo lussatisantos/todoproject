c146c2553cfdcb276aceac17cf31af86c646b801
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-03 17:07:24 +0100

    .

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    console.log('funcionou')
-})
+
+    let deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e) {
+        
+        e.preventDefault();
+
+        let delLink = $(this).attr('href');
+        let result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+    
+});
